734ecb56971a6fd756e28e1690e1711897d5b665
Melissa Kipp <melissakipp.az@gmail.com>
2023-02-05 11:25:45 -0700

    Add nav

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Header from './header';
+
 import './globals.css'
 
 export default function RootLayout({ children }) {
@@ -8,7 +10,10 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
-// import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+// import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from './page.module.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Truth Bomb Social</h1>
+  <>
+    <main>
+      <h1>Home page</h1>
     </main>
-  )
+  </>
+
+
+  );
 }
